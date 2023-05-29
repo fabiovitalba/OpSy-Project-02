8945c02bd3ad6bd74be33ca5791eb9cc9d6568f9
Fabio.Vitalba <fabvit9@gmail.com>
2023-05-29 22:32:00 +0200

    Implement main.c assignment functions

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 
 #define FILENAME "jobs.txt"
 #define N_THREADS 3
+#define VERBOSE 1
 
 /**
  * Struct defining the parameters passed to a thread composed by:
@@ -73,12 +74,59 @@ void run_prime(struct thread_params *params, char *job_parameters);
 void *thread(void *parameters);
 
 int main() {
-    // STUDENT CODE GOES HERE
+    FILE *file = open_file("jobs.txt");
+    pthread_mutex_t global_mutex;
+    pthread_mutex_init(&global_mutex, NULL);
+
+    pthread_t threads[N_THREADS];
+
+    struct thread_params **params = malloc(sizeof(struct thread_params) * N_THREADS);
+    for(int i = 0; i < N_THREADS; i++) {
+        params[i] = malloc(sizeof(struct thread_params));
+        params[i]->jobs_file = file;
+        params[i]->mutex = &global_mutex;
+        params[i]->thread_id = i + 1;
+
+        threads[i] = malloc(sizeof(pthread_t));
+        if (VERBOSE) {
+            printf("launching thread %d\n",params[i]->thread_id);
+        }
+        pthread_create(&(threads[i]),NULL,thread,params[i]);
+    }
+    
+    for(int i = 0; i < N_THREADS; i++) {
+        if (VERBOSE) {
+            printf("waiting for thread %d\n",params[i]->thread_id);
+        }
+        pthread_join(threads[i],NULL);
+        free(params[i]);
+    }
+
+    free(params);
+    pthread_mutex_destroy(&global_mutex);
+    if (VERBOSE) {
+        printf("program completed.\n");
+    }
+
     return 0;
 }
 
 void *thread(void *parameters) {
-    // STUDENT CODE GOES HERE
+    struct thread_params *params = parameters;
+    char job_type = -1;
+    while(job_type != 0) {
+        // Shortly lock the mutex, read the next line and release the mutex again.
+        pthread_mutex_lock(params->mutex);
+        char* job_params = get_next_job(params,&job_type);
+        pthread_mutex_unlock(params->mutex);
+
+        // If a valid job was acquired, execute the job.
+        // Mutex locking is done inside each job, based on necessity.
+        if (job_type > 0) {
+            run_job(params,job_type,job_params);
+            free(job_params);
+        }
+    }
     return NULL;
 }
 
@@ -129,17 +177,95 @@ void run_job(struct thread_params *params, char job_type, char *job_parameters) 
 }
 
 void run_sleep(struct thread_params *params, char *job_parameters) {
-    // STUDENT CODE GOES HERE
+    int seconds;
+    sscanf(job_parameters, "%d", &seconds);
+
+    if (seconds > 0) {
+        pthread_mutex_lock(params->mutex);
+        printf("Thread %d: Running sleep(%d)\n",params->thread_id,seconds);
+        pthread_mutex_unlock(params->mutex);
+
+        job_sleep(seconds);
+
+        pthread_mutex_lock(params->mutex);
+        printf("Thread %d: Job terminated.\n",params->thread_id);
+        pthread_mutex_unlock(params->mutex);
+    } else {
+        pthread_mutex_lock(params->mutex);
+        printf("Thread %d: Error parsing sleep job parameters\n",params->thread_id);
+        pthread_mutex_unlock(params->mutex);
+    }
 }
 
 void run_math(struct thread_params *params, char *job_parameters) {
-    // STUDENT CODE GOES HERE
+    float operand_a, operand_b;
+    char operation;
+    sscanf(job_parameters, "%f%c%f", &operand_a, &operation, &operand_b);
+    int operation_valid = 0;
+
+    switch(operation) {
+        case '+':
+        case '-':
+        case '*':
+        case '/': {
+            if ((operand_a != 0) || (operand_b != 0)) {
+                pthread_mutex_lock(params->mutex);
+                printf("Thread %d: Running math(%f %c %f)\n",params->thread_id,operand_a,operation,operand_b);
+                pthread_mutex_unlock(params->mutex);
+                float result = job_math(operand_a,operand_b,operation);
+                operation_valid = 1;
+                pthread_mutex_lock(params->mutex);
+                printf("Thread %d: Job terminated. Result: %f\n",params->thread_id,result);
+                pthread_mutex_unlock(params->mutex);
+            }
+            break;
+        }
+    }
+
+    if (operation_valid == 0) {
+        pthread_mutex_lock(params->mutex);
+        printf("Thread %d: Error parsing math job parameters\n",params->thread_id);
+        pthread_mutex_unlock(params->mutex);
+    }
 }
 
 void run_factorization(struct thread_params *params, char *job_parameters) {
-    // STUDENT CODE GOES HERE
+    int number, size;
+    sscanf(job_parameters,"%d",&number);
+    if (number > 0) {
+        pthread_mutex_lock(params->mutex);
+        printf("Thread %d: Running factorization(%d)\n",params->thread_id,number);
+        pthread_mutex_unlock(params->mutex);
+        int *factors = job_factorization(number, &size);
+        pthread_mutex_lock(params->mutex);
+        printf("Thread %d: Job terminated. Result: ",params->thread_id);
+        for(int i = 0; i < size; i++) {
+            printf("%d ",factors[i]);
+        }
+        printf("\n");
+        pthread_mutex_unlock(params->mutex);
+        free(factors);
+    } else {
+        pthread_mutex_lock(params->mutex);
+        printf("Thread %d: Error parsing factorization job parameters\n",params->thread_id);
+        pthread_mutex_unlock(params->mutex);
+    }
 }
 
 void run_prime(struct thread_params *params, char *job_parameters) {
-    // STUDENT CODE GOES HERE
+    int number;
+    sscanf(job_parameters,"%d",&number);
+    if (number > 0) {
+        pthread_mutex_lock(params->mutex);
+        printf("Thread %d: Running prime(%d)\n",params->thread_id,number);
+        pthread_mutex_unlock(params->mutex);
+        int next_prime = job_prime(number);
+        pthread_mutex_lock(params->mutex);
+        printf("Thread %d: Job terminated. Result: %d\n",params->thread_id,next_prime);
+        pthread_mutex_unlock(params->mutex);
+    } else {
+        pthread_mutex_lock(params->mutex);
+        printf("Thread %d: Error parsing prime job parameters\n",params->thread_id);
+        pthread_mutex_unlock(params->mutex);
+    }
 }
